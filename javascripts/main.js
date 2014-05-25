2c17a53c57fed4de301e7170a077de93e7face20
Jui-Shan Liang <jsliang.tw@gmail.com>
2014-05-25 14:18:58 +0800

    Refactor javascript

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,40 @@
 (function() {
-  var client_height, header_cover, header_cover_height, header_top;
+  var aboutJS;
 
-  header_cover = window.document.getElementById("cover");
+  aboutJS = {
+    makeCover: function(cover_selector) {
+      /*
+      stretch the height of "cover" object to browser inner height
+      
+      Usage example:
+          <div id="cover">...</div>
+          <script>
+          window.aboutJS.makeCover(window.document.getElementById("cover"));
+          </script>
+      */
 
-  client_height = window.innerHeight || document.documentElement.clientHeight;
+      var client_height, cover, cover_height, cover_style_height, header_top;
 
-  header_cover_height = header_cover.getBoundingClientRect().height;
-
-  if (header_cover_height < client_height) {
-    header_top = header_cover.getBoundingClientRect().top;
-    if (header_top > 0) {
-      header_cover_height = client_height - header_top;
-      header_cover.style.height = header_cover_height + "px";
+      cover = document.querySelector(cover_selector);
+      if (cover != null) {
+        client_height = window.innerHeight || document.documentElement.clientHeight;
+        cover_height = cover.getBoundingClientRect().height;
+        if (cover_height < client_height) {
+          header_top = cover.getBoundingClientRect().top;
+          if (header_top > 0) {
+            cover_style_height = client_height - header_top;
+            return cover.style.height = cover_style_height + "px";
+          }
+        }
+      }
     }
-  }
+  };
+
+  /*
+  top-level function for other scripts to use
+  */
+
+
+  window.aboutJS = aboutJS;
 
 }).call(this);
